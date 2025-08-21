990a5c2117f730b01dd419e7f1e35148aec7abc0
mansi-nirwal <manshinirwal19@gmail.com>
2025-08-21 11:54:27 +0530

    var basics

---
+++ Variables/var.js
@@ -0,0 +1,45 @@
+//let var const
+
+
+var num = 2334;
+
+var num = 334;
+
+const num =23;
+
+// Declaration and Initialization
+
+var first_name; //declaration
+
+var name = "Mansi"; // initialization
+
+/*
+var properties;
+
+window me add hota hai
+
+function scoped hota hai 
+
+firse declare kar sakte hai same name se and error nahi aayega 
+*/
+
+
+const name="Mansi Nirwal";
+//can't change const value its fixed
+
+
+//Scope(global, block, functional)
+
+function ab(){
+    var n=23;
+}//functional 
+
+var n=23; // global scope 
+
+{
+    let n=23; //block scope 
+
+}
+
+
+
